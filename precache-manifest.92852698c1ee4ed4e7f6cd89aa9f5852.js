7d9d5ce669c7f0fa73e2ab902464ce645acc7961
Erhan Arda <erhanarda7@gmail.com>
2020-05-27 00:44:43 +0300

    Updates

---
+++ precache-manifest.92852698c1ee4ed4e7f6cd89aa9f5852.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72c5ba1e71827d4b1f8173d81cc31da5",
+    "url": "/metrobi/index.html"
+  },
+  {
+    "revision": "eef9400d9d031fd32d37",
+    "url": "/metrobi/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f49fb2a54031e820da36",
+    "url": "/metrobi/static/css/main.9f1883ed.chunk.css"
+  },
+  {
+    "revision": "eef9400d9d031fd32d37",
+    "url": "/metrobi/static/js/2.77db55c5.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/metrobi/static/js/2.77db55c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f49fb2a54031e820da36",
+    "url": "/metrobi/static/js/main.6a77068e.chunk.js"
+  },
+  {
+    "revision": "5dfe17aa1ca77a2abac9",
+    "url": "/metrobi/static/js/runtime-main.f7751780.js"
+  }
+]);
